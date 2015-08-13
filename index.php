04d8ff82cd40d9528d47a42a5f63c946ed3151ba
Web-God <alvardsandz@gmail.com>
2015-08-13 12:51:49 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	include("functions.php");
+	ptr($_GET);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Movies</title>
+</head>
+<body>
+	
+</body>
+</html>
